64c37c42737bae5694b075477b01b6abbeec950a
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-24 21:20:35 +0300

    feat: Создать схему и модель карточки

---
+++ models/card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cardSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  likes: {
+    type: mongoose.Schema.Type.ObjectId,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
