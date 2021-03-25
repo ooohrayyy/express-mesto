409a9276fb7acf28643d3aed096d506f5be4a380
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-25 14:54:15 +0300

    feat: Настроить роутинг POST-запросов к /users

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,12 @@ usersRouter.get('/:id', (req, res) => {
     .catch((err) => res.status(500).send({ message: err }));
 });
 
+usersRouter.post('/', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err }));
+});
+
 module.exports = usersRouter;
