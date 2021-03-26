1697492607c8a9c9815843850fb5aa39757e4165
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-26 21:24:34 +0300

    misc: Добавить привет код-ревью

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ! Привет команде код-ревью! Пока было не очень сложно: надеюсь, всё получилось!
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
