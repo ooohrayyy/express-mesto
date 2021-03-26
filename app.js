0894deab27ef449f3fc01879753597b5b8d3d937
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-26 20:41:28 +0300

    feat: Добавить роут 404

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use((req, res, next) => {
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
