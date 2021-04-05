22abf93ca3942aeaadf03eaf7b521a712584d0b0
Boris Nikitashov <ooohrayyy@gmail.com>
2021-04-05 13:02:42 +0300

    feat: Добавить значения по умолчанию в схему пользователя

--- models/user.js
+++ models/user.js
@@ -4,22 +4,22 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   about: {
     type: String,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => /^(https?:\/\/)(www\.)?([\da-z-.]+)\.([a-z.]{2,6})[\da-zA-Z-._~:?#[\]@!$&'()*+,;=/]*\/?#?$/.test(v),
     },
-    required: true,
   },
   email: {
     type: String,
