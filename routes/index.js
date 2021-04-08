283330b28125ecdd62bc61b699cbaab5e822f8b3
Boris Nikitashov <ooohrayyy@gmail.com>
2021-04-08 19:58:53 +0300

    fix: Исправить обработку ошибок на роуте index

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 
 const auth = require('../middlewares/auth.js');
+
 const usersRouter = require('./users.js');
 const {
   createUser,
   login,
 } = require('../controllers/users.js');
 const cardsRouter = require('./cards.js');
+
+const NotFoundError = require('../errors/not-found-err.js');
 
 router.post( // Создать пользователя
   '/signup',
@@ -34,8 +37,8 @@ router.post( // Залогинить пользователя
 );
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
-router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+router.use('*', () => {
+  throw new NotFoundError('Страница не найдена');
 });
 
 module.exports = router;
