eea463b42e2e380b659cbe3e9a2a35617734cbd9
Boris Nikitashov <ooohrayyy@gmail.com>
2021-04-08 10:47:55 +0300

    feat: Валидировать на подходе запросы регистрации и авторизации

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const auth = require('../middlewares/auth.js');
 const usersRouter = require('./users.js');
@@ -8,8 +9,29 @@ const {
 } = require('../controllers/users.js');
 const cardsRouter = require('./cards.js');
 
-router.post('/signup', createUser);
-router.post('/signin', login);
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object.keys({
+      name: Joi.string().min(2).max(30).required(),
+      about: Joi.string().min(2).max(30).required(),
+      avatar: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    }),
+  }),
+  createUser,
+);
+router.post(
+  '/signin',
+  celebrate({
+    body: Joi.object.keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    }),
+  }),
+  login,
+);
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
 router.use('*', (req, res) => {
