d84c132d57ebd69bf2cc581dc987f49ae6b90cfb
Boris Nikitashov <ooohrayyy@gmail.com>
2021-04-18 11:01:45 +0300

    feat: Создать логгер ошибок

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -7,3 +7,10 @@ const requestLogger = expressWinston.logger({
   ],
   format: winston.format.json(),
 });
+
+const errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.File({ filename: 'error.log' }),
+  ],
+  format: winston.format.json(),
+});
