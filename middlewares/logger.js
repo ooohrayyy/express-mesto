df7d78c4ed4ac8441112f2aaa9d0e9f8617156d4
Boris Nikitashov <ooohrayyy@gmail.com>
2021-04-18 11:00:20 +0300

    feat: Создать логгер запросов

---
+++ middlewares/logger.js
@@ -0,0 +1,9 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
+const requestLogger = expressWinston.logger({
+  transports: [
+    new winston.transports.File({ filename: 'request.log' }),
+  ],
+  format: winston.format.json(),
+});
